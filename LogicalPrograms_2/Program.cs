b42baf9f16476c35ac07b6fe4fba5b1a3ccbba09
Shweta Bodake <shwetabodake8108@gmail.com>
2023-02-05 22:22:38 +0530

    [Shweta] Welcome message for logical Programs Of NUint is displayed

---
+++ LogicalPrograms_2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms_2
+{
+class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs of Nunit ");
+        }
+    }
+}
+
