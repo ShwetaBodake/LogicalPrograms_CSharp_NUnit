7e03daf5d6059507e965522e398676b5cc3e43fd
Shweta Bodake <shwetabodake8108@gmail.com>
2023-02-06 15:07:28 +0530

    [Shweta] Modified Square root of non negative number is calculated

--- LogicalPrograms_2/Program.cs
+++ LogicalPrograms_2/Program.cs
@@ -6,7 +6,7 @@ class Program
         {
             Console.WriteLine("Welcome to Logical Programs of Nunit ");
             Console.WriteLine("Enter the option to choose the program 1.Vending Machine \n2.DayOfWeek" +
-                " \n3.TemperaturConversion \n4.Monthly Payment");
+                " \n3.TemperaturConversion \n4.Monthly Payment \n5.Find square root" );
             
                 
             int option = Convert.ToInt32(Console.ReadLine());
@@ -23,6 +23,9 @@ class Program
                     break;
                 case 4:
                     monthlyPayment.monthlyPaymentCalculate();
+                    break;
+                case 5:
+                    squareRoot.squareRootDisplay();
                     break;
                 default:
                     Console.WriteLine("Enter option wihin given option");
---
+++ LogicalPrograms_2/squareRoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LogicalPrograms_2
+{
+	public class squareRoot
+	{
+		public static void squareRootDisplay()
+		{
+            //-----------------UC5_Square root of number --------------------------------//
+            Console.WriteLine("Enter the number:");
+            double Number = Convert.ToInt32(Console.ReadLine());
+            if(Number<0)
+            {
+                Console.WriteLine("Enter the number should greater than zero");
+            }
+            else
+            {
+                double sqrt = Math.Sqrt(Number);
+                Console.WriteLine("The square root of enetred number is {0}", sqrt);
+            } 
+        }
+    }
+}
+
