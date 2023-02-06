e3ceb4b8356e5c969ab44d5e2456f3d4adc8afcf
Shweta Bodake <shwetabodake8108@gmail.com>
2023-02-06 14:32:10 +0530

    [Shweta] Modified Day in week is found of given date

---
+++ LogicalPrograms_2/DayOfWeek.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace LogicalPrograms_2
+{
+	public class DayOfWeek
+    {
+        public static int d, m, y;
+
+        public static void dayofweekDisplay()
+		{
+            //----------------------UC2_Day of entered date with date,month ,year------------------------//
+           // public int d, m, y;
+                Console.WriteLine("Enter the date");
+            Console.WriteLine("Enter the day");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the month");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the year");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The given date is {0}/{1}/{2}", d, m, y);
+            int y0 = y - (14 - m) / 12;
+                                    int x = y0 + y0 / 4 - 0 / 100 + y0 / 400;
+                                    int m0 = m + 12 * ((14 - m) / 12) - 2;
+                                    int d0 = (d + x + (31 * m0) / 12) % 7;
+                                    switch (d0)
+                                    {
+                                        case 0:
+                                            Console.WriteLine("The day is Monday");
+                                            break;
+                                        case 1:
+                                            Console.WriteLine("The day is Tuesaday");
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("The day is Wednesday");
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("The day is Thursday");
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("The day is Friday");
+                                            break;
+                                        case 5:
+                                            Console.WriteLine("The day is Saturday");
+                                            break;
+                                        case 6:
+                                            Console.WriteLine("The day is Sunday");
+                                            break;
+                                        default:
+                                            break;
+                                    }
+        }
+    }
+}
+
--- LogicalPrograms_2/Program.cs
+++ LogicalPrograms_2/Program.cs
@@ -5,38 +5,19 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs of Nunit ");
-            Console.WriteLine("Welcome to Vendor Machine");
-            //———Vending machine——————————//
-            int[] notes = { 1, 2, 5, 10, 50, 100, 500, 1000 };
-            Console.WriteLine("Enter the note for change:");
-            int Rs = Convert.ToInt32(Console.ReadLine());
-            int count_note = 0;
-
-            int[] count = new int[8];
-            while (Rs != 0)
+            Console.WriteLine("Enter the option to choose the program 1.Vending Machine \n2.DayOfWeek");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                int Arrlength = notes.Length;
-                for (int i = Arrlength - 1; i >= 0; i--)
-                {
-                    if (notes[i] <= Rs)
-                    {
-                        count[i] = count[i] + Rs / notes[i];
-                        Rs = Rs % notes[i];
-                    }
-                }
-                for (int i = 7; i >= 0; i--)
-                {
-                    if (count[i] != 0)
-                    {
-                        Console.WriteLine("notes of {0} is of count {1}", notes[i], count[i]);
-                    }
-                }
-                //--------------number of notes are counted-------------------------------------------//
-                for (int i = 0; i < 8; i++)
-                {
-                    count_note += count[i];
-                }
-                Console.WriteLine("Total count of note is:{0}", count_note);
+                case 1:
+                    Vendingmachine.MinimumNotesDisplay();
+                    break;
+                case 2:
+                    DayOfWeek.dayofweekDisplay();
+                    break;
+                default:
+                    Console.WriteLine("Enter option wihin given option");
+                    break;
             }
         }
     }
